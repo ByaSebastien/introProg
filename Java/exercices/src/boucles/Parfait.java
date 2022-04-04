5dbf51841f11cb555f6d1757e04d2950471e7ad9
ByaSebastien <94690516+ByaSebastien@users.noreply.github.com>
2022-04-04 11:17:41 +0200

    petite opti
    condition du while

--- Java/exercices/src/boucles/Parfait.java
+++ Java/exercices/src/boucles/Parfait.java
@@ -8,7 +8,7 @@ public class Parfait {
 		int div=2,nb=6,somme=1;
 
 		while (nb<4000){
-			while (div<=Math.pow(nb, 0.5)) {
+			while (div<=Math.pow(nb, 0.5) && somme<nb) {
 				if (nb%div==0) {
 					if (div!=Math.pow(nb, 0.5)) {
 						somme=somme+div+(nb/div);
